f42a0ec60d74b24e0ec74978970f9dfc9fb4a0c8
devdevaraj <devarajperayil327@gmail.com>
2025-06-04 12:38:44 +0000

    Message

--- src/components/expose-port.tsx
+++ src/components/expose-port.tsx
@@ -4,6 +4,8 @@ import toast from "react-hot-toast";
 import { useParams } from "react-router-dom";
 
 function ExposePort({ vmID }: { vmID: string }) {
+ console.log(vmID);
+
  const [popup, setPopup] = useState(false);
  const [ports, setPorts] = useState<{ [key: string]: string }[]>([]);
  const [port, setPort] = useState<number | null>(null);
@@ -34,32 +36,32 @@ function ExposePort({ vmID }: { vmID: string }) {
    }).catch(console.log);
  }
 
- console.log(ports, port);
-
  return (
   <>
-   <button onClick={() => setPopup(!popup)}>
-    port
+   <button className="text-2xl" onClick={() => setPopup(!popup)}>
+    ⌘
    </button>
    {popup && <main className="fixed inset-0 z-30 bg-[#2226] flex items-center justify-center">
-    <section className="h-4/6 w-4/6 bg-blue-950 rounded-lg relative shadow-2xl p-4">
-     <aside onClick={() => setPopup(!popup)} className="absolute top-2 right-4">
-      X
+    <section className="max-w-full min-w-[40rem] bg-blue-950 rounded-lg relative shadow-2xl p-4">
+     <aside onClick={() => setPopup(!popup)} className="absolute top-2 right-4 text-2xl">
+      🗙
      </aside>
      <section>
-      <aside className="flex">
+      <h2 className="text-3xl text-center">Expose ports</h2>
+      <aside className="flex w-full px-20 py-10 gap-6">
        <input
-        className="bg-white text-black"
+        placeholder="eg: 8000"
+        className="bg-white text-black h-8 rounded-sm w-4/5"
         type="number"
         onInput={e => setPort(Number((e.target as HTMLInputElement).value))} />
        <button
-        className="bg-blue-700"
+        className="bg-blue-700 h-8 px-4 rounded-sm w-1/5"
         onClick={exposeHandler}>
         expose
        </button>
       </aside>
       <aside>
-       <table>
+       <table className="table-auto w-full rounded-sm mb-4">
         <thead>
          <tr>
           <th>No</th>
@@ -74,10 +76,20 @@ function ExposePort({ vmID }: { vmID: string }) {
           <tr key={i}>
            <td>{i + 1}</td>
            <td>{e.vm}</td>
-           <td>{e.subdomain}</td>
+           <td>
+            <a
+             target="_blank"
+             className="text-blue-500 underline"
+             href={`https://${e.subdomain.slice(0, e.subdomain.length - 1)}`}
+            >
+             {`https://${e.subdomain.slice(0, e.subdomain.length - 1)}`}
+            </a>
+           </td>
            <td>{e.port}</td>
            <td>
-            <button onClick={() => deleteHandler(e._id)}>delete</button>
+            <button className="text-xl" onClick={() => deleteHandler(e._id)}>
+             🗑
+            </button>
            </td>
           </tr>
          ))}
